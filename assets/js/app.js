207b5884e8b10f95f25f971c8f1e48e5c997bd40
Björn Bartels <bartels@dragon-projects.net>
2015-10-30 23:04:55 +0100

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,5 +6,20 @@ jQuery.noConflict();
 jQuery(document).ready(function () {
 	
 	// ... your actions here ...
+
+	jQuery('A.fancybox.ajax').each(function(){
+		//console.debug(this);
+		jQuery(this).fancybox({
+			maxWidth	: 800,
+			maxHeight	: 600,
+			fitToView	: false,
+			width		: '99%',
+			height		: '99%',
+			autoSize	: false,
+			closeClick	: false,
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+	}); 
 	
 });
