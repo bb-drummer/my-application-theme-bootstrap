a7b85ac62cefc4aee066bb2d724b4090ac236870
Björn Bartels <bartels@dragon-projects.net>
2015-11-03 16:07:29 +0100

    update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,12 @@ jQuery.noConflict();
 
 jQuery(document).ready(function () {
 	
-	// ... your actions here ...
+	/* jQuery('TABLE.table').dataTable( {
+		renderer	: 'bootstrap',
+		language	: {
+			url	: '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
+		}
+	} ); */
+	
 	
 });
