8b78b82e84fff6db798635db553f14ed63f57f4b
Björn Bartels <bartels@dragon-projects.net>
2016-01-20 11:54:44 +0100

    update datatables, cta-xhr js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,33 @@
  */
 jQuery.noConflict();
 
-jQuery(document).ready(function () {
+jQuery(document).ready(function ($) {
 	
-	/* jQuery('TABLE.table').dataTable( {
+	jQuery('.datatable').dataTable( {
 		renderer	: 'bootstrap',
 		language	: {
 			url	: '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
 		}
-	} ); */
+	} );
+
+	$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']";
+	$ajaxAction = ".btn-cta-xhr";
 	
-	
+	jQuery($ajaxButtons).addClass('fancybox.ajax');
+	jQuery($ajaxButtons).each(function(){
+		// console.debug(this);
+		jQuery(this).fancybox({
+			minWidth	: 800,
+			maxWidth	: 800,
+			maxHeight	: 600,
+			fitToView	: false,
+			width		: '99%',
+			height		: '99%',
+			autoSize	: true,
+			autoCenter	: true,
+			closeClick	: false,
+			openEffect	: 'none',
+			closeEffect	: 'none'
+		});
+	}); 
 });
