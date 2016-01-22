aeb50b56bb5f19df4f3d98ccfeae746907d101cd
Björn Bartels <bartels@dragon-projects.net>
2016-01-22 15:51:33 +0100

    update xhr table

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,94 +5,92 @@ jQuery.noConflict();
 
 var initDatatables = function () {
 	jQuery('.datatable').each(function (idx, elm) { 
-		$table = jQuery(this);
-		
-		var datatableOptions = {
-			renderer : 'bootstrap',
-			language : {
-				url : '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
-			},
-			ajax : null
-		};
+		var $table = jQuery(this),
+			datatableOptions = {
+				renderer : 'bootstrap',
+				language : {
+					url : '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
+				},
+				ajax : null
+			}
+		;
 		
 		// has data source?
 		var $src = jQuery($table).data("src");
-		console.log($src)
 		if ($src) {
 			datatableOptions.ajax = {
 				url : $src,
 				type : "POST"
-			}
+			};
 		}
 		
 		// get columns
-		var $columns = [];
+		var $columns = false;
 		$table.find('THEAD TH').each(function () {
 			var columnname = jQuery(this).data("column");
-			console.log(columnname)
 			if (columnname) {
+				if (!$columns) { $columns = []; }
 				$columns.push({
 					data : columnname
 				});
 			}
 		});
-		if ($table.find('THEAD TH.actions').size() > 0) {
-			$columns.push(null);
-			/*$columns.push({
-				data : "user_id"
-			});*/
-		}
-		if ($columns.length > 0) {
+		if ($columns) {
+			if ($table.find('THEAD TH.actions').size() > 0) {
+				$columns.push(null);
+		        $columnDefs = [ {
+		            targets : -1,
+		            data : "user_id",
+		            sortable : false,
+		            searchable : false,
+		            render: function ( data, type, full, meta ) {
+		            	console.log(arguments, this);
+		            	return "-custom-";
+		            }
+		        } ];
+		        datatableOptions.columnDefs = $columnDefs;
+			}
 			datatableOptions.columns = $columns;
 			// actions' columns
-	        $columnDefs = [ {
-	            targets : -1,
-	            data : "user_id",
-	            sortable : false,
-	            searchable : false,
-	            render: function ( data, type, full, meta ) {
-	            	console.log(arguments, this);
-	            	return "-custom-";
-	            }
-	        } ];
-	        datatableOptions.columnDefs = $columnDefs;
 		}
 		
-		console.log(datatableOptions)
 		// init table
+		console.log(datatableOptions);
 		$table.dataTable(datatableOptions);
 	});
 };
 
 var initCTAXHR = function () {
-	$fancyboxDefaults = {
-		minWidth	: 720,
-		maxWidth	: 720,
-		maxHeight	: 572,
-		fitToView	: false,
-		width		: '99%',
-		height		: '99%',
-		autoSize	: true,
-		autoCenter	: true,
-		closeClick	: false,
-		openEffect	: 'none',
-		closeEffect	: 'none',
-		modal		: true,
-		helpers 	: {
-			overlay		: {
-				closeClick : false
+	var $fancyboxDefaults = {
+			minWidth	: 720,
+			maxWidth	: 720,
+			maxHeight	: 572,
+			fitToView	: false,
+			width		: '99%',
+			height		: '99%',
+			autoSize	: true,
+			autoCenter	: true,
+			closeClick	: false,
+			openEffect	: 'none',
+			closeEffect	: 'none',
+			modal		: true,
+			helpers 	: {
+				overlay		: {
+					closeClick : false
+				}
 			}
-		}
-	};
-	$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']";
-	$ajaxCTAOpen = "A.btn-cta-xhr";
-	$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages";
-	$ajaxForms = ".fancybox-wrap .form-xhr";
+		},
+		$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']",
+		$ajaxCTAOpen = "A.btn-cta-xhr",
+		$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages",
+		$ajaxForms = ".fancybox-wrap .form-xhr"
+	;
 	
 	jQuery($ajaxCTAOpen).each(function(){
-		$this = jQuery(this);
-		$this.addClass('fancybox.ajax');
-		$actioncontext = jQuery.data($this, "actioncontext");
+		var $this = jQuery(this),
+			$this.addClass('fancybox.ajax'),
+			$actioncontext = jQuery.data($this, "actioncontext");
+		
 		if ($actioncontext != "") {
 			this.actioncontext = $actioncontext;
 			jQuery.fancybox.actioncontext = $actioncontext;
