0bb484649ec29e69a2314660ac54da86948d5012
Björn Bartels <bartels@dragon-projects.net>
2016-01-22 11:32:11 +0100

    update user datatable/view

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,101 +3,118 @@
  */
 jQuery.noConflict();
 
-jQuery(document).ready(function ($) {
-	
-	jQuery('.datatable').dataTable( {
-		renderer	: 'bootstrap',
-		language	: {
-			url	: '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
+var datatables = [],
+	modals = []
+;
+
+var initDatatables = function () {
+	jQuery('.datatable').each(function (idx, elm) { 
+		$table = jQuery(this);
+		var datatableOptions = {
+			renderer : 'bootstrap',
+			language : {
+				url : '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
+			},
+			ajax : null
+		};
+		$src = jQuery.data($table, "src");
+		if ($src != "") {
+			datatableOptions.ajax = {
+				url : $src,
+				type : "POST"
+			}
 		}
-	} );
+		$table.dataTable(datatableOptions);
+	});
+};
 
-	var initCTAXHR = function () {
-		$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']";
-		$ajaxCTAOpen = "A.btn-cta-xhr";
-		$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages";
-		$ajaxForms = ".fancybox-wrap .form-xhr";
-		
-		jQuery($ajaxCTAOpen).addClass('fancybox.ajax');
-		
-		jQuery($ajaxCTAOpen).each(function(){
-			// console.debug(this);
-			jQuery(this).fancybox({
-				minWidth	: 720,
-				maxWidth	: 720,
-				maxHeight	: 572,
-				fitToView	: false,
-				width		: '99%',
-				height		: '99%',
-				autoSize	: true,
-				autoCenter	: true,
-				closeClick	: false,
-				openEffect	: 'none',
-				closeEffect	: 'none',
-				modal		: true,
-				helpers 	: {
-					overlay		: {
-						closeClick : false
-					}
-				}
-			});
-		}); 
-	
-		jQuery('BODY').on('submit', $ajaxForms, {}, function (oEvent) {
-			var formURL = (this.action),
-				form = $(this),
-				formdata = form.serializeArray();
-			
-			formdata.push( (jQuery('.fancybox-wrap INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null );
-			
-			jQuery.fancybox.showLoading();
-			
-			jQuery.ajax({
-				headers : {
-					'Accept' : 'text/html',
-					'X-Fancybox' : 'true'
-				},
-				type	: "POST",
-				cache	: false,
-				url		: formURL,
-				data	: formdata,
-				success	: function (data) {
-					
-					jQuery.fancybox(data, {
-						minWidth	: 720,
-						maxWidth	: 720,
-						maxHeight	: 572,
-						fitToView	: false,
-						width		: '99%',
-						height		: '99%',
-						autoSize	: true,
-						autoCenter	: true,
-						closeClick	: false,
-						openEffect	: 'none',
-						closeEffect	: 'none',
-						modal		: true,
-						helpers 	: {
-							overlay		: {
-								closeClick : false
-							}
-						}
-					});
-					jQuery('.flashmessages').first().parents('.fancybox-skin').removeClass('fancybox-skin');
-
-				}
-			});
-			
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
-		
-		jQuery('BODY').on('click', $ajaxCTAClose, {}, function (oEvent) {
-			jQuery.fancybox.close();
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
+var initCTAXHR = function () {
+	$fancyboxDefaults = {
+		minWidth	: 720,
+		maxWidth	: 720,
+		maxHeight	: 572,
+		fitToView	: false,
+		width		: '99%',
+		height		: '99%',
+		autoSize	: true,
+		autoCenter	: true,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none',
+		modal		: true,
+		helpers 	: {
+			overlay		: {
+				closeClick : false
+			}
+		}
 	};
+	$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']";
+	$ajaxCTAOpen = "A.btn-cta-xhr";
+	$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages";
+	$ajaxForms = ".fancybox-wrap .form-xhr";
+	
+	jQuery($ajaxCTAOpen).each(function(){
+		$this = jQuery(this);
+		$this.addClass('fancybox.ajax');
+		$actioncontext = jQuery.data($this, "actioncontext");
+		if ($actioncontext != "") {
+			this.actioncontext = $actioncontext;
+			jQuery.fancybox.actioncontext = $actioncontext;
+		}
+		$fancyboxDefaults = jQuery.extend($fancyboxDefaults, {
+			beforeClose : function () {
+				console.log( 
+					jQuery.fancybox.actioncontext, 
+					(jQuery.fancybox.actioncontext) ? jQuery(jQuery.fancybox.actioncontext).datatable().data() : undefined
+				);
+			}
+		})
+		jQuery(this).fancybox($fancyboxDefaults);
+	}); 
+
+	jQuery('BODY').on('submit', $ajaxForms, {}, function (oEvent) {
+		var formURL = (this.action),
+			form = $(this),
+			formdata = form.serializeArray();
+		
+		formdata.push( (jQuery('.fancybox-wrap INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null );
+		
+		jQuery.fancybox.showLoading();
+		
+		jQuery.ajax({
+			headers : {
+				'Accept' : 'text/html',
+				'X-Fancybox' : 'true'
+			},
+			type	: "POST",
+			cache	: false,
+			url		: formURL,
+			data	: formdata,
+			success	: function (data) {
+				
+				jQuery.fancybox(data, $fancyboxDefaults);
+				jQuery('.flashmessages').first().parents('.fancybox-skin').removeClass('fancybox-skin');
+				jQuery('.datatable').ajax.reload(function ( tabledata ) {
+					console.log( jQuery.fancybox.actioncontext, tabledata );
+				}, true);
+				
+			}
+		});
+		
+		oEvent.preventDefault();
+		oEvent.stopPropagation();
+		return (false);
+	});
+	
+	jQuery('BODY').on('click', $ajaxCTAClose, {}, function (oEvent) {
+		jQuery.fancybox.close();
+		oEvent.preventDefault();
+		oEvent.stopPropagation();
+		return (false);
+	});
+};
+	
+jQuery(document).ready(function ($) {
+	initDatatables();
 	initCTAXHR();
 });
