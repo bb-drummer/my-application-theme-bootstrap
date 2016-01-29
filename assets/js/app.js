7bc3b61aebf3c2cef9f4426318206b245cd00ad7
Björn Bartels <bartels@dragon-projects.net>
2016-01-29 01:20:45 +0100

    update modals

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,241 +15,260 @@
 
 jQuery.noConflict();
 
-(function ($, d, w) {
+(function ($, doc, win) {
 	
-	var initDatatables = function () {
-		$('.datatable').each(function (idx, elm) { 
-			var $table = $(this),
-				datatableOptions = {
-					renderer : 'bootstrap',
-					language : {
-						url : '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
-					},
-					ajax : null
+	var $doc = $(doc),
+		
+		// datatables
+		initDatatables = function () {
+			$('.datatable').each(function (idx, elm) { 
+				var $table = $(this),
+					datatableOptions = {
+						renderer : 'bootstrap',
+						language : {
+							url : '//cdn.datatables.net/plug-ins/1.10.9/i18n/German.json'
+						},
+						ajax : null
+					}
+				;
+				
+				// has data source?
+				var $src = $($table).data("src");
+				if ($src) {
+					datatableOptions.ajax = {
+						url : $src,
+						type : "POST"
+					};
 				}
+				
+				// get columns
+				var $columns = false;
+				$table.find('THEAD TH').each(function () {
+					var columnname = $(this).data("column");
+					if (columnname) {
+						if (!$columns) { $columns = []; }
+						$columns.push({
+							data : columnname
+						});
+					}
+				});
+				if ($columns) {
+					if ($table.find('THEAD TH.actions').size() > 0) {
+						$columns.push(null);
+				        $columnDefs = [ {
+				            targets : -1,
+				            data : "_actions_",
+				            sortable : false,
+				            searchable : false,
+				            /*render: function ( data, type, full, meta ) {
+				            	console.log(arguments, this);
+				            	return "-custom-";
+				            }*/
+				        } ];
+				        datatableOptions.columnDefs = $columnDefs;
+					}
+					datatableOptions.columns = $columns;
+					// actions' columns
+				}
+				
+				// init table
+				console.log(datatableOptions);
+				$table.dataTable(datatableOptions);
+			});
+		},
+	
+		/* 
+		// (fancy-box) modals
+		initCTAXHR_FancyBox = function () {
+			var $body = $('BODY'),
+				$fancyboxDefaults = {
+					minWidth	: 720,
+					maxWidth	: 720,
+					maxHeight	: 572,
+					fitToView	: false,
+					width		: '99%',
+					height		: '99%',
+					autoSize	: true,
+					autoCenter	: true,
+					closeClick	: false,
+					openEffect	: 'none',
+					closeEffect	: 'none',
+					modal		: true,
+					helpers 	: {
+						overlay		: {
+							closeClick : false
+						}
+					}
+				},
+				$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']",
+				$ajaxCTAOpen = "A.btn-cta-xhr",
+				$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages",
+				$ajaxForms = ".fancybox-wrap .form-xhr"
 			;
 			
-			// has data source?
-			var $src = $($table).data("src");
-			if ($src) {
-				datatableOptions.ajax = {
-					url : $src,
-					type : "POST"
-				};
-			}
-			
-			// get columns
-			var $columns = false;
-			$table.find('THEAD TH').each(function () {
-				var columnname = $(this).data("column");
-				if (columnname) {
-					if (!$columns) { $columns = []; }
-					$columns.push({
-						data : columnname
-					});
+			$($ajaxCTAOpen).each(function(){
+				var $this = $(this),
+					$actioncontext = $.data($this, "actioncontext");
+		
+				$this.addClass('fancybox.ajax');
+				if ($actioncontext != "") {
+					this.actioncontext = $actioncontext;
+					$.fancybox.actioncontext = $actioncontext;
 				}
-			});
-			if ($columns) {
-				if ($table.find('THEAD TH.actions').size() > 0) {
-					$columns.push(null);
-			        $columnDefs = [ {
-			            targets : -1,
-			            data : "_actions_",
-			            sortable : false,
-			            searchable : false,
-			            /*render: function ( data, type, full, meta ) {
-			            	console.log(arguments, this);
-			            	return "-custom-";
-			            }*/
-			        } ];
-			        datatableOptions.columnDefs = $columnDefs;
-				}
-				datatableOptions.columns = $columns;
-				// actions' columns
-			}
-			
-			// init table
-			console.log(datatableOptions);
-			$table.dataTable(datatableOptions);
-		});
-	};
-	
-	/* var initCTAXHR_FancyBox = function () {
-		var $body = $('BODY'),
-			$fancyboxDefaults = {
-				minWidth	: 720,
-				maxWidth	: 720,
-				maxHeight	: 572,
-				fitToView	: false,
-				width		: '99%',
-				height		: '99%',
-				autoSize	: true,
-				autoCenter	: true,
-				closeClick	: false,
-				openEffect	: 'none',
-				closeEffect	: 'none',
-				modal		: true,
-				helpers 	: {
-					overlay		: {
-						closeClick : false
+				$fancyboxDefaults = $.extend($fancyboxDefaults, {
+					beforeClose : function () {
+						console.log( 
+							$.fancybox.actioncontext, 
+							($.fancybox.actioncontext) ? $($.fancybox.actioncontext).datatable().data() : undefined
+						);
 					}
-				}
-			},
-			$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']",
-			$ajaxCTAOpen = "A.btn-cta-xhr",
-			$ajaxCTAClose = ".fancybox-wrap .btn-cta-xhr-close, .fancybox-wrap .flashmessages",
-			$ajaxForms = ".fancybox-wrap .form-xhr"
-		;
+				})
+				$(this).fancybox($fancyboxDefaults);
+			}); 
 		
-		$($ajaxCTAOpen).each(function(){
-			var $this = $(this),
-				$actioncontext = $.data($this, "actioncontext");
-	
-			$this.addClass('fancybox.ajax');
-			if ($actioncontext != "") {
-				this.actioncontext = $actioncontext;
-				$.fancybox.actioncontext = $actioncontext;
-			}
-			$fancyboxDefaults = $.extend($fancyboxDefaults, {
-				beforeClose : function () {
-					console.log( 
-						$.fancybox.actioncontext, 
-						($.fancybox.actioncontext) ? $($.fancybox.actioncontext).datatable().data() : undefined
-					);
-				}
-			})
-			$(this).fancybox($fancyboxDefaults);
-		}); 
-	
-		$body.on('submit', $ajaxForms, {}, function (oEvent) {
-			var formURL = (this.action),
-				form = $(this),
-				formdata = form.serializeArray();
-			
-			formdata.push( ($('.fancybox-wrap INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null );
-			
-			$.fancybox.showLoading();
-			
-			$.ajax({
-				headers : {
-					'Accept' : 'text/html',
-					'X-Fancybox' : 'true'
-				},
-				type	: "POST",
-				cache	: false,
-				url		: formURL,
-				data	: formdata,
-				success	: function (data) {
-					
-					$.fancybox(data, $fancyboxDefaults);
-					$('.flashmessages').first().parents('.fancybox-skin').removeClass('fancybox-skin');
-					$('.datatable').ajax.reload(function ( tabledata ) {
-						console.log( $.fancybox.actioncontext, tabledata );
-					}, true);
-					
-				}
+			$body.on('submit', $ajaxForms, {}, function (oEvent) {
+				var formURL = (this.action),
+					form = $(this),
+					formdata = form.serializeArray();
+				
+				formdata.push( ($('.fancybox-wrap INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null );
+				
+				$.fancybox.showLoading();
+				
+				$.ajax({
+					headers : {
+						'Accept' : 'text/html',
+						'X-Fancybox' : 'true'
+					},
+					type	: "POST",
+					cache	: false,
+					url		: formURL,
+					data	: formdata,
+					success	: function (data) {
+						
+						$.fancybox(data, $fancyboxDefaults);
+						$('.flashmessages').first().parents('.fancybox-skin').removeClass('fancybox-skin');
+						$('.datatable').ajax.reload(function ( tabledata ) {
+							console.log( $.fancybox.actioncontext, tabledata );
+						}, true);
+						
+					}
+				});
+				
+				oEvent.preventDefault();
+				oEvent.stopPropagation();
+				return (false);
 			});
 			
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
-		
-		$body.on('click', $ajaxCTAClose, {}, function (oEvent) {
-			$.fancybox.close();
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
-	}; */
+			$body.on('click', $ajaxCTAClose, {}, function (oEvent) {
+				$.fancybox.close();
+				oEvent.preventDefault();
+				oEvent.stopPropagation();
+				return (false);
+			});
+		}, */
 	
-	var initCTAXHR = function () {
-		var $body = $('BODY'),
-			$modalDefaults = {
-				show: true
-			},
-			$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']",
-			$ajaxCTAOpen = "A.btn-cta-xhr",
-			$ajaxCTAClose = ".modal-content .btn-cta-xhr-close, .modal-content .alert, .modal-content .close",
-			$ajaxForms = ".modal-content .form-xhr"
-		;
-		
-		$body.on('click', $ajaxCTAOpen, {}, function (oEvent) {
-			var $this = $(this),
-				$actioncontext = $.data($this, "actioncontext");
-			
-			$.ajax({
-				headers : {
-					'Accept' : 'text/html'
+		// (bootstrap) modals
+		initCTAXHRModals = function () {
+			var $body = $('BODY'),
+				$modalDefaults = {
+					show: true
 				},
-				type	: "GET",
-				cache	: false,
-				url		: $this.attr('href'),
-				success	: function (data) {
-					
-					$(data).modal($modalDefaults);
-					$('#'+$actioncontext).dataTable().api().ajax.reload(function ( tabledata ) {
-						console.log( tabledata );
-					}, true);
-					
-				}
+				$ajaxButtons = "A.btn[href*='add'], A.btn[href*='edit'], A.btn[href*='details'], A.btn[href*='delete']",
+				$ajaxCTAOpen = "A.btn-cta-xhr.cta-xhr-modal",
+				$ajaxCTAClose = ".modal-content .btn-cta-xhr-close, .modal-content .alert, .modal-content .close",
+				$ajaxForms = ".modal-content .form-xhr"
+			;
+			
+			//
+			// modal triggers
+			//
+			$body.on('click', $ajaxCTAOpen, {}, function (oEvent) {
+				var $this = $(this),
+					$actioncontext = $.data($this, "actioncontext");
+				
+				$.ajax({
+					headers : {
+						'Accept' : 'text/html',
+						'X-layout' : 'modal'
+					},
+					type	: "GET",
+					cache	: false,
+					url		: $this.attr('href'),
+					success	: function (data) {
+						
+						$(data).modal($modalDefaults);
+						$('#'+$actioncontext).dataTable().api().ajax.reload(function ( tabledata ) {
+							// console.log( tabledata );
+						}, true);
+						
+					}
+				});
+				
+				oEvent.preventDefault();
+				oEvent.stopPropagation();
+				return (false);
+				
+			}); 
+		
+			//
+			// modal forms
+			//
+			$body.on('submit', $ajaxForms, {}, function (oEvent) {
+				var formURL = (this.action),
+					form = $(this),
+					formdata = form.serializeArray();
+				
+				formdata.push( 
+					(form.find('.modal-dialog INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null 
+				);
+				
+				$.ajax({
+					headers : {
+						'Accept' : 'text/html',
+						'X-layout' : 'modal'
+					},
+					type	: "POST",
+					cache	: false,
+					url		: formURL,
+					data	: formdata,
+					success	: function (data) {
+						
+						$('.modal').modal('hide');
+						$(data).modal($modalDefaults);
+						$('.datatable').dataTable().api().ajax.reload(function ( tabledata ) {
+							// console.log( tabledata );
+						}, true);
+						
+					}
+				});
+				
+				oEvent.preventDefault();
+				oEvent.stopPropagation();
+				return (false);
+			});
+	
+			//
+			// modal close
+			//
+			$body.on('click', $ajaxCTAClose, {}, function (oEvent) {
+				$('.modal').modal('hide');
+				//$.modal.destroy();
+				oEvent.preventDefault();
+				oEvent.stopPropagation();
+				return (false);
+			});
+	
+			$body.on('hidden.bs.modal', '.modal', {}, function (oEvent) {
+				$('.modal').remove();
 			});
 			
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-			
-		}); 
-	
-		$body.on('submit', $ajaxForms, {}, function (oEvent) {
-			var formURL = (this.action),
-				form = $(this),
-				formdata = form.serializeArray();
-			
-			formdata.push( ($('.modal-dialog INPUT[name=del].btn').size() > 0) ? {name: 'del', value: 'delete'} : null );
-			
-			$.ajax({
-				headers : {
-					'Accept' : 'text/html',
-					'X-Fancybox' : 'true'
-				},
-				type	: "POST",
-				cache	: false,
-				url		: formURL,
-				data	: formdata,
-				success	: function (data) {
-					
-					$('.modal').modal('hide');
-					$(data).modal($modalDefaults);
-					$('.datatable').dataTable().api().ajax.reload(function ( tabledata ) {
-						console.log( tabledata );
-					}, true);
-					
-				}
-			});
-			
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
-
-		$body.on('click', $ajaxCTAClose, {}, function (oEvent) {
-			$('.modal').modal('hide');
-			//$.modal.destroy();
-			oEvent.preventDefault();
-			oEvent.stopPropagation();
-			return (false);
-		});
-
-		$body.on('hidden.bs.modal', '.modal', {}, function (oEvent) {
-			$('.modal').remove();
-		});
+		}
+	;
 		
-	};
-		
-	$(document).ready(function () {
+	$doc.ready(function () {
 		initDatatables();
-		initCTAXHR();
+		initCTAXHRModals();
 	});
 
 })(jQuery, document, window);
