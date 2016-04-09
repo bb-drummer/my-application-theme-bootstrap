12bf0d3124ea86b74fec48164103d012deb839c3
Björn Bartels <bartels@dragon-projects.net>
2016-04-09 02:14:18 +0200

    update doc info

--- config.php
+++ config.php
@@ -6,12 +6,12 @@
  *   - theme created by/taken from [Almsaeed Studio](https://almsaeedstudio.com)
  *   
  * @package		[MyApplication]
- * @package		BB's Zend Framework 2 Components
- * @package		Theme 'Basic'
- * @author		Björn Bartels <development@bjoernbartels.earth>
+ * @subpackage	BB's Zend Framework 2 Components
+ * @subpackage	Theme 'Basic'
+ * @author		Björn Bartels <coding@bjoernbartels.earth>
  * @link		https://gitlab.bjoernbartels.earth/groups/themes
  * @license		http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
- * @copyright	copyright (c) 2016 Björn Bartels <development@bjoernbartels.earth>
+ * @copyright	copyright (c) 2016 Björn Bartels <coding@bjoernbartels.earth>
  */
 
 return array(
